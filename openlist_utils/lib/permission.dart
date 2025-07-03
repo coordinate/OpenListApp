8472c97813d01a7e3dc7614c2a47e3df5b1bfec3
Yu Fang <newfarry@126.com>
2025-07-03 13:27:53 +0800

    修复安卓音视频等媒体权限问题

--- openlist_utils/lib/permission.dart
+++ openlist_utils/lib/permission.dart
@@ -4,10 +4,10 @@ import 'package:openlist_utils/toast.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 Future<void> requestStorage(BuildContext ctx) async {
-  PermissionStatus status = await Permission.storage.request();
   PermissionStatus status2 = await Permission.videos.request();
   PermissionStatus status3 = await Permission.audio.request();
   PermissionStatus status4 = await Permission.photos.request();
+  PermissionStatus status = await Permission.storage.request();
   if (status.isGranted) {
 // 存储权限已授权，可以进行文件读写操作
     show_success("Granted",ctx);
