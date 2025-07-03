99cc5f0d1254fd87d289bf961a117c2a67ba7db9
Yu Fang <newfarry@126.com>
2025-07-03 12:13:25 +0800

    修复安卓网络权限

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
+//        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
